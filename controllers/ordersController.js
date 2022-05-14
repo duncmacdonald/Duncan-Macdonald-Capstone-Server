ae440d3396811c463c3f8684e2cb7348ccdfd1c0
Duncan Macdonald <dunc.macdonald@gmail.com>
2022-05-13 20:20:27 -0700

    server returns the order history of one user

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -5,18 +5,7 @@ exports.myOrders = (req, res) => {
 
     knex('orders').select('*').where({user_id: req.query.user_id}).whereNot({status: 'checkout'}).then(data => res.status(200).json(data));
 }
-//   knex('orders')
 
-//     .join('restaurants', 'orders.restaurant_id', '=', 'restaurants.id')
-//     .select('orders.id', 'orders.user_id', 'orders.time', 'orders.status', 'restaurants.name')
-//     .then((data) => {
-        
-//       res.status(200).json(data);
-//     })
-//     .catch((err) =>
-//       res.status(400).send(`Error retrieving orders: ${err}`)
-//     );
-// };
 
 exports.returnDishes = (req, res) => {
     console.log(req);
